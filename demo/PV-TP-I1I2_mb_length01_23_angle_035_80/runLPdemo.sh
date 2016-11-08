db2acc00f9e44c6f80ea985b4385c642ae9d3f0e
Michal Hradis <michal.hradis@gmail.com>
2016-11-08 16:10:26 +0100

    LP and text deblur real-time demo.

---
+++ demo/PV-TP-I1I2_mb_length01_23_angle_035_80/runLPdemo.sh
@@ -0,0 +1,6 @@
+export PYTHONPATH=$PYTHONPATH:/home/ihradis/projects/2016-07-07_JPEG/cnn-image:/home/ihradis/projects/2015-07-29_realCNNDeconvolution/CODE-2/src/
+
+SCRIPT=/home/ihradis/projects/2016-07-07_JPEG/cnn-image/bin/feed_forward_cnn.py
+
+$SCRIPT -c config.yaml -d deploy.prototxt -cw net_iter_400000.caffemodel -l live.q20 -v
+#$SCRIPT -c config.yaml -d deploy.prototxt -cw S14_19_real_130000.model -l live.q20
