bc2bb446ffcfbf8b846c78467abe91571064d270
Michal Hradis <ihradis@fit.vutbr.cz>
2017-03-30 13:56:38 +0200

    Add demo for text deblurring

---
+++ demo/text/run.sh
@@ -0,0 +1,14 @@
+
+
+~/projects/2016-07-07_JPEG/cnn-image-new/bin/demo.py \
+ -d deploy.prototxt -m S14_19_real_130000.model \
+ -c 430 --input-layer data --output-layer CONVX_15
+
+exit
+
+export PYTHONPATH=$PYTHONPATH:/home/ihradis/projects/2016-07-07_JPEG/cnn-image:/home/ihradis/projects/2015-07-29_realCNNDeconvolution/CODE-2/src/
+
+SCRIPT=/home/ihradis/projects/2016-07-07_JPEG/cnn-image/bin/feed_forward_cnn.py
+
+#$SCRIPT -c config.yaml -d deploy.prototxt -cw net_iter_400000.caffemodel -l live.q20
+$SCRIPT -c config.yaml -d deploy.prototxt -cw S14_19_real_130000.model -l live.q20
